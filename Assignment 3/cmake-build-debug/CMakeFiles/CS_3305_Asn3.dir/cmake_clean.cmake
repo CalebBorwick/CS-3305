c539959ce7c5aa12f6aa3c51d74eb9df0f947eed
Caleb Borwick <50631296+CalebBorwick@users.noreply.github.com>
2020-06-23 14:56:45 -0700

    Add files via upload

---
+++ Assignment 3/cmake-build-debug/CMakeFiles/CS_3305_Asn3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS_3305_Asn3.dir/account.c.o"
+  "CMakeFiles/CS_3305_Asn3.dir/main.c.o"
+  "CS_3305_Asn3"
+  "CS_3305_Asn3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CS_3305_Asn3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
