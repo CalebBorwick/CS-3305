ab8211702b13040febc54f049af0d171073ad321
Caleb Borwick <50631296+CalebBorwick@users.noreply.github.com>
2020-06-23 14:53:46 -0700

    Add files via upload

---
+++ Assignment 2/Part 2/cmake-build-debug/CMakeFiles/CS_3305_Asn2_Part2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS_3305_Asn2_Part2.dir/part2.c.o"
+  "CS_3305_Asn2_Part2"
+  "CS_3305_Asn2_Part2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CS_3305_Asn2_Part2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
